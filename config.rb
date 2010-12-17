c30b3f8dbcc430a976eac98b20bd15da0e527c73
jorgepedret <jorgepedret@gmail.com>
2010-12-16 22:21:06 -0800

    Initial changes. Second initial commit

--- config.rb
+++ config.rb
@@ -7,3 +7,6 @@ images_dir = "img"
 javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
+
+# Uncomment the following line to get a compacted css file 
+# output_style = :compressed
